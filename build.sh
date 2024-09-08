1a072246ec5550aec45a25e84611edc58e726613
DragonWork <97512+DragonWork@users.noreply.github.com>
2024-09-08 15:10:40 +0000

    chore: add support for Go 1.22.7+ and override config.yaml if it does not exist

--- build.sh
+++ build.sh
@@ -10,3 +10,8 @@ go build -ldflags '-s -w' -o "$BASEDIR"/postfix-tlspol .
 # Build and (in case the file is already linked) reload systemd service unit
 sed -e "s!%%BASEDIR%%!${BASEDIR}!g" ../utils/postfix-tlspol.service.template > ../postfix-tlspol.service
 systemctl daemon-reload
+
+# Create config.yaml if it does not exist
+if ! [ -f "$BASEDIR"/config.yaml ]; then
+    cp -a "$BASEDIR"/config.example.yaml "$BASEDIR"/config.yaml
+fi
