330f77ce45a8e7db4ac53e3cee469de90f818f69
DragonWork <97512+DragonWork@users.noreply.github.com>
2025-05-08 15:00:21 +0000

    build: improve version tag derivation

--- main.go
+++ main.go
@@ -3,15 +3,42 @@ package main
 import (
 	_ "embed"
 	"github.com/Zuplu/postfix-tlspol/internal"
+	"regexp"
+	"runtime/debug"
+	"strings"
 )
 
 var (
-	Version string
+	Version string = "undefined"
 	//go:embed LICENSE
 	LicenseText string
 	//go:embed configs/config.default.yaml
 	defaultConfigYaml []byte
 )
+
+func cleanVersion(raw string) string {
+	re := regexp.MustCompile(`^v?(\d+\.\d+\.\d+)(?:-[\d.]+-[a-f0-9]+)?(?:\+dirty)?$`)
+	match := re.FindStringSubmatch(raw)
+	if len(match) >= 2 {
+		base := match[1]
+		if strings.Contains(raw, "+dirty") || strings.Contains(raw, "-0.") {
+			return base + "-dev"
+		}
+		return base
+	}
+	return "dev"
+}
+
+func init() {
+	if info, ok := debug.ReadBuildInfo(); ok {
+		if info.Main.Version != "" {
+			Version = cleanVersion(info.Main.Version)
+		}
+	}
+	if strings.HasPrefix(Version, "v") {
+		Version = Version[1:]
+	}
+}
 
 func main() {
 	tlspol.SetDefaultConfig(&defaultConfigYaml)
